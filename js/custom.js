57581dc7a0ce787963273769f1ea98f21aeda9b7
capytyler <133891698+capytyler@users.noreply.github.com>
2024-02-11 23:26:55 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -43,28 +43,3 @@ if (mediaQuery.matches) {
 }
 
 
-document.addEventListener("DOMContentLoaded",function(){
-    var loading = new TimelineMax();
-    
-
-    
-    loading.fromTo(
-        ".name-animation",
-        { opacity: 0, x: -100 },
-        { opacity: 1, x: 0, duration: 2,
-          ease: 'elastic.out(1,0.3)',
-          transformOrigin: 'left top',
-          stagger: {
-            each: 0.1,
-            from: 'random'
-          }
-        }
-      );
-      
-      gsap.fromTo(
-        ".job-title",
-        { y: 20, opacity: 0, scale: 1.2 },
-        { delay: 2, opacity: 1, y: 0, duration: 3, scale: 1, ease: 'elastic.out(1,0.3)' }
-      );
-
-})
